9290cc7c8ff1af28cceb8d9d3dd01f11f1bf5209
Saw Xue Zheng <sawxuezheng01@gmail.com>
2017-02-11 20:35:38 -0500

    Added Sensor Chart, Fixed Widget Container to and Sample Fetcher to allow for new thread

---
+++ PyQt GUI/SensorChart.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtChart import QChart, QBarSet, QBarSeries, QBarCategoryAxis, QChartView, QValueAxis
+from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout
+from PyQt5.QtGui import QPixmap, QPainter, QColor
+
+
+class SensorChart(QWidget):
+
+    #constructor
+    def __init__(self, sample):
+        super().__init__()
+        self.samples = sample
+        self.initUI()
+
+    def initUI(self):
+
+        self.set0 = QBarSet("Sensors")
+
+        for sample in self.samples:
+            self.set0.append(sample)
+
+        self.series = QBarSeries()
+        self.series.append(self.set0)
+
+        self.chart = QChart()
+        self.chart.setTitle("Sensor Values")
+        self.chart.addSeries(self.series)
+        self.categories = []
+
+        count = 1
+        for sample in self.samples:
+            self.categories.append("S" + str(count))
+            count += 1
+
+        self.axis_x = QBarCategoryAxis()
+        self.axis_x.append(self.categories)
+
+        self.chart.setAxisX(self.axis_x, self.series)
+
+        self.axis_y = QValueAxis()
+        self.chart.setAxisY(self.axis_y, self.series)
+        self.axis_y.setRange(0,3000)
+
+        self.chartView = QChartView(self.chart)
+        self.chartView.setRenderHint(QPainter.Antialiasing)
+
+        self.grid = QGridLayout()
+        self.grid.addWidget(self.chartView)
+        self.setLayout(self.grid)
+
+        #self.resize(800,500)
+
+    ## SLOTS ##
+    def updateSeries(self, samples):
+
+        #create a new bar set
+        self.set_update = QBarSet("Sensors")
+
+        for sample in samples:
+            self.set_update.append(sample)    
+
+        # Get the series from charview
+        self.series.clear()
+        self.series.append(self.set_update)
+        self.chartView.update()
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    sample = [100]*16
+    w = SensorChart(sample)
+    w.show()
+    sys.exit(app.exec_())
--- PyQt GUI/WidgetContainer.py
+++ PyQt GUI/WidgetContainer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
+
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QThread, pyqtSignal
+
 from ConnectWidget import ConnectWidget
 from MaskInfoWidget import MaskInfoWidget
 from SampleFetcher import SampleFetcher
+
 
 ### class WidgetContainer: 
 ### Description : Widget to contain every other widget
@@ -19,7 +23,11 @@ class WidgetContainer(QWidget):
         self.cW = ConnectWidget()
         self.miW = MaskInfoWidget()
 
-        #self.fetcher =
+        #Connect cross object signals and slots
+        self.cW.connected_sig.connect(self.startFetcherThread)
+        ##self.cW.disconnected_sig.connect(self.stopFetcherThread)
+        self.cW.connect_fail_sig.connect(self.connect_fail_handler)
+
 
 
     def initLayout(self):
@@ -32,9 +40,41 @@ class WidgetContainer(QWidget):
 
         #Assign layout to self
         self.setLayout(self.grid)
+        self.resize(1000,500)
+
+    
+    ### Start of SLOT ###
+    def startFetcherThread(self):
+
+        #Instantiate a fetcher and a thread
+        self.fetcher = SampleFetcher(self.cW.getConnectedPort())
+        self.thread = QThread()
+
+        #Connect signals so that process is called when thread is started
+        self.thread.started.connect(self.fetcher.process)
+
+        self.fetcher.doneSampling.connect(lambda : self.miW.updateSamples(self.fetcher.getSamples()))
+        self.fetcher.doneSampling.connect(self.miW.update)
+        self.fetcher.doneSampling.connect(self.thread.quit)
+        self.fetcher.doneSampling.connect(lambda : self.miW.getChart().updateSeries(self.fetcher.getSamples()))
+        self.thread.finished.connect(self.restartFetcherThread)
+
+        #Move fetcher to the thread and start the process
+        self.fetcher.moveToThread(self.thread)
+        self.thread.start()
 
 
+    def restartFetcherThread(self):
+        self.thread.start()
 
+    def stopFetcherThread(self):
+        self.thread.quit()
+
+    #Not implemented
+    #Description: Handle when connection fail
+    def connect_fail_handler(self):
+        pass
+    ### End of SLOT ###
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
